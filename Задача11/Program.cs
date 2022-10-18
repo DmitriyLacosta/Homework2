32778c478d4649a3c1c9f19998a5e5325010a63b
Дмитрий <dkovaloff@gmail.com>
2022-10-18 06:51:17 +0500

    задача 2 Программисты

---
+++ Задача11/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 11 HARD - необязательная: Напишите программу, которая принимает на вход целое число любой разрядности число и удаляет вторую цифру слева направо этого
+// числа.
+
+Console.WriteLine("Программа удаляет 2-ю цифру заданного числа");
+Console.WriteLine("введите положительное число: ");
+int a = int.Parse(Console.ReadLine()!);
+//int count = 1;
+//int b = a;
+if (a > 9)
+{
+    while (a > 1000)
+    {
+        a = a / 10;
+        //count = count + 1;
+    }
+    int one = a / 100;
+    int two = a % 10;
+    //int result = a - (count * 10);
+    Console.WriteLine("получившееся число: "+ one + two);
+
+}
+else
+    Console.WriteLine("второй цифры нет");
+
---
+++ Задача2/Program.cs
@@ -0,0 +1,34 @@
+﻿//робот считает программистов
+
+
+Console.WriteLine("Программа правильно говорит количество программистов");
+Console.WriteLine("введите количество программистов: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = a;
+if(a == 11 || a == 12 || a == 13 || a == 14 || a == 15 || a == 16 || a == 17 || a == 18 || a == 19 || a == 20)
+{Console.WriteLine("в офисе " + a); Console.WriteLine("программистов");
+return;
+}
+
+while (b > 10)
+    {
+        b = b % 10;
+    }
+if(b == 1)
+{
+Console.WriteLine("в офисе " + a); Console.WriteLine("программист");
+return;
+}
+if(b == 2 || b == 3 || b == 4  && b != 5)
+{
+ Console.WriteLine("в офисе " + a); Console.WriteLine("программиста");
+return;
+}
+
+if(b >= 5 || b == 0 || b == 6 || b == 7 || b == 8 || b == 9)
+{
+ Console.WriteLine("в офисе " + a); Console.WriteLine("программистов");
+return;
+}
+
+
